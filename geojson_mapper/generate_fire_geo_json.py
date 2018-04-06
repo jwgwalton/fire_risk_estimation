42bb1c93d7ea7471852739bffca523d322a81590
Joseph Walton <jwgwalton@gmail.com>
2018-04-06 13:43:02 +0100

    move outputs for visualisations

--- geojson_mapper/generate_fire_geo_json.py
+++ geojson_mapper/generate_fire_geo_json.py
@@ -2,7 +2,7 @@ from geojson_mapper import GeoJsonMapper
 
 geojson_file_path = "geojson/london.geojson"
 fire_data_file_path = "fire_data/fires_by_postcode.csv"
-output_geojson_path = "geojson/london_fires.geojson"
+output_geojson_path = "../data_visualisations/london_fires.geojson"
 
 geojson_mapper = GeoJsonMapper(geojson_file_path, fire_data_file_path, "Number of Fires", "Name")
 
